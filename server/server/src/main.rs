2fbb73c6f080b2cfaf942cd47b60f3c4af01f769
赵光建 <zhaoguangjian@cyberkl.com>
2025-07-04 01:10:17 +0800

    static file server works

--- server/server/src/main.rs
+++ server/server/src/main.rs
@@ -4,19 +4,23 @@ use axum::Router;
 use migration::{Migrator, MigratorTrait};
 use sea_orm::{ConnectOptions, Database};
 use server::{AppState, article};
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     log4rs::init_file("./config/log4rs.yml", Default::default())?;
     let mut opt =
-        ConnectOptions::new("postgres://zhaogj_user:zhaogj_password@127.0.0.1:5432/zhaogj_kb_db");
+        ConnectOptions::new("postgres://zhaogj_user:zhaogj_password@127.0.0.1:5432/zhaogj_db");
     opt.max_connections(1);
     opt.sqlx_logging_level(log::LevelFilter::Trace);
     let pg_conn = Database::connect(opt).await?;
     Migrator::up(&pg_conn, None).await?;
     let app_state = AppState { pg_conn };
-    let app = Router::new().nest("/api/article", article::routers(app_state));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:2020").await?;
+
+    let app = Router::new()
+        .fallback_service(ServeDir::new("../../ui/dist"))
+        .nest("/api/article", article::routers(app_state));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
     log::info!("listening on {}", listener.local_addr()?);
     axum::serve(
         listener,
