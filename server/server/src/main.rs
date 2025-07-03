0256bf300abbf351e2d6579beb223661c72edb2b
赵光建 <zhaoguangjian@cyberkl.com>
2025-07-04 01:39:02 +0800

    add sqlite

--- server/server/src/main.rs
+++ server/server/src/main.rs
@@ -9,11 +9,16 @@ use tower_http::services::ServeDir;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     log4rs::init_file("./config/log4rs.yml", Default::default())?;
+    // let db_url = "sqlite::memory:";
+    // let db_url = "sqlite://data/zhaogj_db.sqlite";
+    // let pg_conn = Database::connect(db_url).await?;
+
     let mut opt =
         ConnectOptions::new("postgres://zhaogj_user:zhaogj_password@127.0.0.1:5432/zhaogj_db");
-    opt.max_connections(1);
+    opt.max_connections(5);
     opt.sqlx_logging_level(log::LevelFilter::Trace);
     let pg_conn = Database::connect(opt).await?;
+
     Migrator::up(&pg_conn, None).await?;
     let app_state = AppState { pg_conn };
 
