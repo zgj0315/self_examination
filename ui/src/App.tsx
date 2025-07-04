4766156eba3fb90fc6536d6b99ce731093282d4b
赵光建 <zhaoguangjian@cyberkl.com>
2025-07-04 12:08:21 +0800

    article create works

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import type { FormProps } from "antd";
+import { Button, Form, Input, message } from "antd";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+type FieldType = {
+  title?: string;
+  content?: string;
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+  console.log("Success:", values);
+  try {
+    const response = axios.post("/api/article/create", values);
+    console.log("create success, response: ", response);
+    message.success("create success");
+  } catch (e) {
+    console.error("create error: ", e);
+    message.error("create error");
+  }
+};
 
-export default App
+const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
+const App: React.FC = () => (
+  <Form
+    name="basic"
+    labelCol={{ span: 8 }}
+    wrapperCol={{ span: 16 }}
+    style={{ maxWidth: 600 }}
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item<FieldType>
+      label="Title"
+      name="title"
+      rules={[{ required: true, message: "Please input title!" }]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item<FieldType>
+      label="Content"
+      name="content"
+      rules={[{ required: true, message: "Please input content!" }]}
+    >
+      <Input.TextArea />
+    </Form.Item>
+
+    <Form.Item label={null}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+);
+
+export default App;
