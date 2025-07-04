9b5ea3971b7bd4b41b36a91aa111ba4368b02265
赵光建 <zhaoguangjian@cyberkl.com>
2025-07-04 11:03:01 +0800

    sqlite works

--- server/server/src/main.rs
+++ server/server/src/main.rs
@@ -1,23 +1,33 @@
-use std::net::SocketAddr;
+use std::{
+    fs::{self, File},
+    net::SocketAddr,
+    path::Path,
+};
 
 use axum::Router;
 use migration::{Migrator, MigratorTrait};
-use sea_orm::{ConnectOptions, Database};
+use sea_orm::Database;
 use server::{AppState, article};
 use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     log4rs::init_file("./config/log4rs.yml", Default::default())?;
-    // let db_url = "sqlite::memory:";
-    // let db_url = "sqlite://data/zhaogj_db.sqlite";
-    // let pg_conn = Database::connect(db_url).await?;
 
-    let mut opt =
-        ConnectOptions::new("postgres://zhaogj_user:zhaogj_password@127.0.0.1:5432/zhaogj_db");
-    opt.max_connections(5);
-    opt.sqlx_logging_level(log::LevelFilter::Trace);
-    let pg_conn = Database::connect(opt).await?;
+    // create sqlite db dir and file
+    let db_dir = Path::new("./data");
+    if !db_dir.exists() {
+        fs::create_dir_all(&db_dir)?;
+        log::info!("create dir: {}", db_dir.to_string_lossy());
+    }
+    let db_path = db_dir.join("zhaogj_db.sqlite");
+    if !db_path.exists() {
+        File::create(&db_path)?;
+        log::info!("create file: {}", db_path.to_string_lossy());
+    }
+    let db_url = format!("sqlite://{}", db_path.to_string_lossy());
+    log::info!("connect to {}", db_url);
+    let pg_conn = Database::connect(db_url).await?;
 
     Migrator::up(&pg_conn, None).await?;
     let app_state = AppState { pg_conn };
