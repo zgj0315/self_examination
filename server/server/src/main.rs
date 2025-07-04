7d5b9383a84488f77a7216fd960e658c0acd3ae5
赵光建 <zhaoguangjian@cyberkl.com>
2025-07-04 11:05:00 +0800

    remove some useless code

--- server/server/src/main.rs
+++ server/server/src/main.rs
@@ -14,29 +14,31 @@ use tower_http::services::ServeDir;
 async fn main() -> anyhow::Result<()> {
     log4rs::init_file("./config/log4rs.yml", Default::default())?;
 
-    // create sqlite db dir and file
     let db_dir = Path::new("./data");
     if !db_dir.exists() {
         fs::create_dir_all(&db_dir)?;
         log::info!("create dir: {}", db_dir.to_string_lossy());
     }
+
     let db_path = db_dir.join("zhaogj_db.sqlite");
     if !db_path.exists() {
         File::create(&db_path)?;
         log::info!("create file: {}", db_path.to_string_lossy());
     }
+
     let db_url = format!("sqlite://{}", db_path.to_string_lossy());
+    let pg_conn = Database::connect(&db_url).await?;
     log::info!("connect to {}", db_url);
-    let pg_conn = Database::connect(db_url).await?;
 
     Migrator::up(&pg_conn, None).await?;
-    let app_state = AppState { pg_conn };
 
+    let app_state = AppState { pg_conn };
     let app = Router::new()
         .fallback_service(ServeDir::new("../../ui/dist"))
         .nest("/api/article", article::routers(app_state));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
     log::info!("listening on {}", listener.local_addr()?);
+
     axum::serve(
         listener,
         app.into_make_service_with_connect_info::<SocketAddr>(),
