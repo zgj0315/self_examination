e9d7dd118dcf99455f89621fc669a64ef55147b7
赵光建 <zhaoguangjian@cyberkl.com>
2025-07-10 21:52:03 +0800

    reorg tsx

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import Article from "./Article.tsx";
 import Log from "./Log.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Article />
     <Log />
   </StrictMode>
 );
