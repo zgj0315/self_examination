caaa6314ec4ec0c10c6d4634e15b7484f1715e37
赵光建 <zhaoguangjian@cyberkl.com>
2025-07-12 00:06:03 +0800

    add log

--- server/server/src/auth.rs
+++ server/server/src/auth.rs
@@ -2,7 +2,7 @@ use std::net::SocketAddr;
 
 use axum::{
     extract::{ConnectInfo, FromRequestParts},
-    http::{StatusCode, header, request::Parts},
+    http::{StatusCode, request::Parts},
 };
 
 pub struct RequireAuth;
@@ -14,18 +14,16 @@ where
     type Rejection = StatusCode;
 
     async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
-        let auth_header = parts
-            .headers
-            .get(header::AUTHORIZATION)
-            .and_then(|value| value.to_str().ok());
-        log::info!("auth_header: {:?}", auth_header);
-
-        let uri = parts.uri.to_string();
-        log::info!("uri: {uri}");
+        // let auth_header = parts
+        //     .headers
+        //     .get(header::AUTHORIZATION)
+        //     .and_then(|value| value.to_str().ok());
+        // log::info!("auth_header: {:?}", auth_header);
 
         match ConnectInfo::<SocketAddr>::from_request_parts(parts, state).await {
             Ok(ConnectInfo(socket_addr)) => {
-                log::info!("socket_addr: {:?}", socket_addr);
+                // log::info!("socket_addr: {:?}", socket_addr);
+                log::info!("{} {} {}", socket_addr.ip(), parts.method, parts.uri);
             }
             Err(e) => {
                 log::error!("get source ip err: {}", e);
