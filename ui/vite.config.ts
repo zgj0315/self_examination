4ef898216678a58f2d593b8dd8eb5cb13ef02c57
赵光建 <zhaoguangjian@cyberkl.com>
2025-07-21 12:10:29 +0800

    https bugfix

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -8,7 +8,9 @@ export default defineConfig({
     port: 5173, 
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8080', 
+        target: 'https://127.0.0.1:8080',
+        changeOrigin: true,
+        secure: false, 
       }
     }
   }
